e3a711d36a40fa5d8bef00585f2b1a1b62499711
ciecclm <clm@imnu.edu.cn>
2018-09-27 08:39:40 +0800

    Initial Commit

---
+++ caculater927Tests/caculater927Tests.swift
@@ -0,0 +1,36 @@
+//
+//  caculater927Tests.swift
+//  caculater927Tests
+//
+//  Created by 朝力萌 on 2018/9/27.
+//  Copyright © 2018年 朝力萌. All rights reserved.
+//
+
+import XCTest
+@testable import caculater927
+
+class caculater927Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
