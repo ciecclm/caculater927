ba69be1260f5fa92329633fc8e5ea8fc8decf0b6
ciecclm <clm@imnu.edu.cn>
2018-09-27 09:18:51 +0800

    计算器初步
    加法运算功能已成

--- caculater927/ViewController.swift
+++ caculater927/ViewController.swift
@@ -9,7 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var temp = 0
+    @IBOutlet var caculaterDisplay: UITextField!
+    
+    @IBAction func button1(_ sender: Any) {
+        caculaterDisplay.text = caculaterDisplay.text!+"1"
+        
+    }
+    
+    @IBAction func button2(_ sender: Any) {
+        caculaterDisplay.text = caculaterDisplay.text!+"2"
+    }
+    
+    @IBAction func button3(_ sender: Any) {
+        caculaterDisplay.text = caculaterDisplay.text!+"3"
+    }
+    
+    @IBAction func buttonCaculator(_ sender: Any) {
+        var sum = 0
+        sum = temp + Int(caculaterDisplay.text!)!
+        caculaterDisplay.text = "\(sum)"
+    }
+    @IBAction func buttonAdd(_ sender: Any) {
+        temp = Int(caculaterDisplay.text!)!
+        caculaterDisplay.text = ""
+    }
+    @IBAction func clear(_ sender: Any) {
+        caculaterDisplay.text = ""
+    }
+    
+   
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
